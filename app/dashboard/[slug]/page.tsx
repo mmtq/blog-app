39b0c52e25f13b29cfe2905e93533b39e474dea5
mmtq <mirtarhimul@gmail.com>
2025-06-05 00:27:36 +0600

    Server Components and Client Components

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,10 +1,12 @@
 type Params = Promise<{ slug: string }>
+import Counter from "@/app/components/Counter"
 
 const page = async ({params}: {params: Params}) => {
     const {slug} = await params
   return (
     <div>
-      <h1>Hellow from slug : {slug}  </h1>
+      <h1>Hellow from slug : {slug} </h1>
+      <Counter />
     </div>
   )
 }
