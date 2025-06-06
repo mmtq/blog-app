b5fcd226eef32ff2ce89fcb6d1f45e6b0d0a5be8
mmtq <mirtarhimul@gmail.com>
2025-06-06 08:55:08 +0600

    Fetching Data with Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
+import { prisma } from "./utils/db";
 
 async function getData() {
-  const items = [
-    {
-      title: "Post 1",
-      content: "Content 1",
-    },
-  ]
-  return items
+  const data = await prisma.blogpost.findMany({
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      imageURL: true,
+      authorName: true,
+      authorImage: true,
+      createdAt: true
+    }
+    })
+  return data
 }
 
 export default async function Home() {
-  const items = await getData()
+  const data = await getData()
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-
+        {data.map((item) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.content}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
