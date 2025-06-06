3823563e8a4e1b0e0f67e3f732790032b9cae544
mmtq <mirtarhimul@gmail.com>
2025-06-06 12:39:38 +0600

    authentication and privacy without middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user){
+    return redirect('/api/auth/register');
+  }
   return (
     <div>
       <h1>Dashboard</h1>
--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,24 +1,37 @@
 import Link from "next/link"
-import { Button } from "../ui/button"
+import { Button, buttonVariants } from "../ui/button"
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-  return (
-    <nav className="py-5 flex items-center justify-between">
-        <div className="flex items-center gap-6">
-            <Link href="/">
-                <h1 className="text-3xl font-semibold">Blog<span className="text-blue-500">MMTQ</span></h1>
-            </Link>
-            <div className="hidden sm:flex items-center gap-6">
-                <Link href="/" className="text-sm font-medium hover:text-blue-500 transition-colors">Home</Link>
-                <Link href="/dashboard" className="text-sm font-medium hover:text-blue-500 transition-colors">Dashboard</Link>
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    return (
+        <nav className="py-5 flex items-center justify-between">
+            <div className="flex items-center gap-6">
+                <Link href="/">
+                    <h1 className="text-3xl font-semibold">Blog<span className="text-blue-500">MMTQ</span></h1>
+                </Link>
+                <div className="hidden sm:flex items-center gap-6">
+                    <Link href="/" className="text-sm font-medium hover:text-blue-500 transition-colors">Home</Link>
+                    <Link href="/dashboard" className="text-sm font-medium hover:text-blue-500 transition-colors">Dashboard</Link>
+                </div>
             </div>
-        </div>
-        <div className="flex items-center gap-4">
-            <Button>Sign In</Button>
-            <Button variant="outline">Sign Up</Button>
-        </div>
-    </nav>
-  )
+            {user ? (
+                <div className="flex items-center gap-4">
+                    <div>
+                        <p>{user.given_name}</p>
+                    </div>
+                    <LogoutLink className={buttonVariants({ variant: "outline" })}>Sign Out</LogoutLink>
+                </div>
+            ) : (
+                <div className="flex items-center gap-4">
+                    <RegisterLink className={buttonVariants({})}>Sign Up</RegisterLink>
+                    <LoginLink className={buttonVariants({ variant: "outline" })}>Sign In</LoginLink>
+                </div>
+            )}
+        </nav>
+    )
 }
 
 export default Navbar
