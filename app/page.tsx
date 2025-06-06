4a81bd7524fb80951810b4ac757f57d6e960384b
mmtq <mirtarhimul@gmail.com>
2025-06-06 08:22:40 +0600

    get started with the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+async function getData() {
+  const items = [
+    {
+      title: "Post 1",
+      content: "Content 1",
+    },
+  ]
+  return items
+}
+
+export default async function Home() {
+  const items = await getData()
   return (
-    <div>
-      <h1>Hellow from Home!</h1>
-      <Link href="/dashboard">Dashboard</Link>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      </div>
     </div>
   );
 }
---
+++ components/general/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+
+const Navbar = () => {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+        <div className="flex items-center gap-6">
+            <Link href="/">
+                <h1 className="text-3xl font-semibold">Blog<span className="text-blue-500">MMTQ</span></h1>
+            </Link>
+            <div className="hidden sm:flex items-center gap-6">
+                <Link href="/" className="text-sm font-medium hover:text-blue-500 transition-colors">Home</Link>
+                <Link href="/dashboard" className="text-sm font-medium hover:text-blue-500 transition-colors">Dashboard</Link>
+            </div>
+        </div>
+        <div className="flex items-center gap-4">
+            <Button>Sign In</Button>
+            <Button variant="outline">Sign Up</Button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
