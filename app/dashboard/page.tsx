79fe5e4f869267d942c990f9839d626314c5f3f4
mmtq <mirtarhimul@gmail.com>
2025-06-06 13:02:41 +0600

    used middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,13 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
 
 const page = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user){
-    return redirect('/api/auth/register');
-  }
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
+        <Link href="/dashboard/create" className={buttonVariants({})}>Create Post</Link>
+      </div>
     </div>
   )
 }
